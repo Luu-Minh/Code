f4907519fc4a33874c1403f24f19127f7cef5874
spacetrap-admin <pmast1234567890@gmail.com>
2025-05-14 10:27:05 +0700

    commit

---
+++ If/Linear/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Nhap so a: ");
+        int a = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("Nhap so b: ");
+        int b = int.Parse(System.Console.ReadLine());
+
+        if (a == 0) {
+            if (b == 0) {
+                System.Console.WriteLine("Phuong trinh co vo so nghiem");
+            } else {
+                System.Console.WriteLine("Phuong trinh vo nghiem");
+
+            }
+        } else {
+            double x = -b / a;
+            System.Console.WriteLine($"Phuong trinh co nghiem x = {x}");
+        }
+    }
+}
